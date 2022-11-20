1fdcef8433c705e09c9d80aacd755196cf244009
ezgihendrickx <ezgihendrickx@gmail.com>
2022-11-20 20:20:24 +0100

    add the details page link

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Car from "./Carousel";
 import Main from "./Main";
 import Intro from "./Intro";
-import Details from "./Details";
 // import "./tailwind.css";
 
 function Home() {
