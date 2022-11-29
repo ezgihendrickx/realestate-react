5d9f2db4c1796d803501049af9a10e58f3e9e108
ezgihendrickx <ezgihendrickx@gmail.com>
2022-11-29 15:41:39 +0100

    improve text

--- src/Details.js
+++ src/Details.js
@@ -36,17 +36,114 @@ function Details(props) {
             <span class="hidden lg:absolute lg:inset-y-0 lg:-left-16 lg:block lg:w-16 lg:bg-gray-100"></span>
 
             <div class="p-8 sm:p-16 lg:p-24">
-              <h2 class="text-2xl font-bold sm:text-3xl">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Tempore, debitis.
-              </h2>
+              <h1 class="text-2xl font-bold sm:text-3xl">Konak Twin Towers</h1>
+              <p class="mt-4 text-gray-600">
+                Konak Twin Towers 1 Project consists of a land area of ​​3,751
+                m2. It consists of 2 blocks and 174 flats in the city center of
+                Alanya. 200 mt. to Mahmutlar center, 20 mt. to the beach. is at
+                a distance. It has a position that dominates the sea and the
+                castle view.
+              </p>
 
               <p class="mt-4 text-gray-600">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aliquid, molestiae! Quidem est esse numquam odio deleniti,
-                beatae, magni dolores provident quaerat totam eos, aperiam
-                architecto eius quis quibusdam fugiat dicta.
+                Common Areas: Indoor Parking Lot, Doorman, 24 Hour Security,
+                Outdoor Swimming Pool, Indoor Swimming Pool, Fitness, Turkish
+                Bath, Steam Room, Massage Room, Sauna, Pool Bar, Children's
+                Playground, Internet, Satellite System
               </p>
+              <a
+                href="#"
+                class="block rounded-lg p-4 shadow-sm shadow-indigo-100"
+              >
+                <div class="mt-2">
+                  <dl>
+                    <div>
+                      <dt class="sr-only">Price</dt>
+
+                      <dd class="text-sm text-gray-500">489.000 €</dd>
+                    </div>
+
+                    <div>
+                      <dt class="sr-only">Address</dt>
+
+                      <dd class="font-medium">
+                        Mahmutlar Mah. D-400 highway BLV. NO:23A ALANYA/ANTALYA{" "}
+                      </dd>
+                    </div>
+                  </dl>
+
+                  <div class="mt-6 flex items-center gap-8 text-xs">
+                    <div class="sm:inline-flex sm:shrink-0 sm:items-center">
+                      <svg
+                        class="h-4 w-4 text-indigo-700"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                        />
+                      </svg>
+
+                      <div class="mt-1.5 sm:ml-3 sm:mt-0">
+                        <p class="text-gray-500">Parking</p>
+
+                        <p class="font-medium">2 spaces</p>
+                      </div>
+                    </div>
+
+                    <div class="sm:inline-flex sm:shrink-0 sm:items-center">
+                      <svg
+                        class="h-4 w-4 text-indigo-700"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                        />
+                      </svg>
+
+                      <div class="mt-1.5 sm:ml-3 sm:mt-0">
+                        <p class="text-gray-500">Bathroom</p>
+
+                        <p class="font-medium">2 rooms</p>
+                      </div>
+                    </div>
+
+                    <div class="sm:inline-flex sm:shrink-0 sm:items-center">
+                      <svg
+                        class="h-4 w-4 text-indigo-700"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                        />
+                      </svg>
+
+                      <div class="mt-1.5 sm:ml-3 sm:mt-0">
+                        <p class="text-gray-500">Bedroom</p>
+
+                        <p class="font-medium">2 rooms</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
               <div>
                 <a
                   href="#"
@@ -58,100 +155,6 @@ function Details(props) {
             </div>
           </div>
         </div>
-        <a href="#" class="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-          <img
-            alt="Home"
-            src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            class="h-56 w-full rounded-md object-cover"
-          />
-
-          <div class="mt-2">
-            <dl>
-              <div>
-                <dt class="sr-only">Price</dt>
-
-                <dd class="text-sm text-gray-500">$240,000</dd>
-              </div>
-
-              <div>
-                <dt class="sr-only">Address</dt>
-
-                <dd class="font-medium">123 Wallaby Avenue, Park Road</dd>
-              </div>
-            </dl>
-
-            <div class="mt-6 flex items-center gap-8 text-xs">
-              <div class="sm:inline-flex sm:shrink-0 sm:items-center">
-                <svg
-                  class="h-4 w-4 text-indigo-700"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                  />
-                </svg>
-
-                <div class="mt-1.5 sm:ml-3 sm:mt-0">
-                  <p class="text-gray-500">Parking</p>
-
-                  <p class="font-medium">2 spaces</p>
-                </div>
-              </div>
-
-              <div class="sm:inline-flex sm:shrink-0 sm:items-center">
-                <svg
-                  class="h-4 w-4 text-indigo-700"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                  />
-                </svg>
-
-                <div class="mt-1.5 sm:ml-3 sm:mt-0">
-                  <p class="text-gray-500">Bathroom</p>
-
-                  <p class="font-medium">2 rooms</p>
-                </div>
-              </div>
-
-              <div class="sm:inline-flex sm:shrink-0 sm:items-center">
-                <svg
-                  class="h-4 w-4 text-indigo-700"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                </svg>
-
-                <div class="mt-1.5 sm:ml-3 sm:mt-0">
-                  <p class="text-gray-500">Bedroom</p>
-
-                  <p class="font-medium">4 rooms</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </a>
       </div>
     </section>
   );
